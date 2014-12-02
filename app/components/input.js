05b24ecd53012629677f338b3f2e395ba40ecb23
Alex Henrie <alexhenrie24@gmail.com>
2014-12-01 20:26:43 -0700

    Debounce search

--- app/components/input.js
+++ app/components/input.js
@@ -1,6 +1,7 @@
 var React = require('react');
 var TokenInput = require('react-tokeninput');
 var ComboboxOption = TokenInput.Option;
+var debounce = require('lodash-node/modern/functions/debounce');
 var request = require('superagent');
 var uniq = require('lodash-node/modern/arrays/uniq');
 var without = require('lodash-node/modern/arrays/without');
@@ -21,6 +22,7 @@ module.exports = React.createClass({
         });
       });
     }
+    this.debouncedSearch = debounce(this.search, 300);
   },
   getInitialState() {
     return {
@@ -34,25 +36,7 @@ module.exports = React.createClass({
     });
   },
   handleInput(userInput) {
-    request.get('/api/terms?query=' + userInput, (error, result) => {
-      if (error) {
-        return;
-      }
-      var selectedOptions = this.state.selected;
-      this.setState({
-        options: result.body.filter(function(term) {
-          for (var i = 0; i < selectedOptions.length; i++) {
-            if (term.name == selectedOptions[i].name) {
-              return false;
-            }
-          }
-          return true;
-        }).map(function(term) {
-          term.name += ' [' + term.id + ']';
-          return term;
-        }),
-      });
-    });
+    this.debouncedSearch(userInput);
   },
   handleSelect(value, combobox) {
     this.setState({
@@ -85,4 +69,28 @@ module.exports = React.createClass({
       );
     });
   },
+  search(userInput) {
+    if (!userInput)
+      return;
+
+    request.get('/api/terms?query=' + userInput, (error, result) => {
+      if (error) {
+        return;
+      }
+      var selectedOptions = this.state.selected;
+      this.setState({
+        options: result.body.filter(function(term) {
+          for (var i = 0; i < selectedOptions.length; i++) {
+            if (term.name == selectedOptions[i].name) {
+              return false;
+            }
+          }
+          return true;
+        }).map(function(term) {
+          term.name += ' [' + term.id + ']';
+          return term;
+        }),
+      });
+    });
+  }
 });
